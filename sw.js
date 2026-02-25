a7c99b9d776fe873e933c6c23bcbd642df966f61
manavvilencetech-rgb <manav.vilencetech@gmail.com>
2026-02-25 18:45:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,34 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
+const CACHE_NAME = "kushira-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icons/icon-72.png",
+  "/icons/icon-96.png",
+  "/icons/icon-144.png",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("/index.html"))
+      );
+    })
+  );
+});
